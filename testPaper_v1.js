8c8456a0c31584c27ae8fb2e2431792ce4475484
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-02 20:50:35 +0800

    add judgement to judge isNan

--- testPaper_v1.js
+++ testPaper_v1.js
@@ -1,4 +1,5 @@
 document.querySelector("#submit").addEventListener("click",function(evt) {
+    window.scroll(0,0);
     evt.preventDefault();
     if(document.getElementById("class").value == "") {
         alert("请输入班级");
@@ -8,7 +9,10 @@ document.querySelector("#submit").addEventListener("click",function(evt) {
     }
     if(document.getElementById("id").value == "") {
         alert("请输入学号");
+    }if(isNaN(document.getElementById("id").value)){
+        alert("请输入数字");
     }else {
         document.getElementById("score").value = comparison();
     }
+    isText();
 });
